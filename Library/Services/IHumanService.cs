074c4d0b207721caaa303f542e39d97f7504ebe0
Mattias Olsson <mattiaso@crepido.com>
2019-11-18 15:05:21 +0100

    Add another service

---
+++ Library/Services/IHumanService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Services
+{
+    public interface IHumanService
+    {
+        IList<Human> Get();
+    }
+
+    public class Human
+    {
+        public string Name { get; set; }
+        public string Occupation { get; set; }
+        public string Location { get; set; }
+    }
+}
