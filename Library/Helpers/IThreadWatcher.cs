a8b16fd572c1b20495a4c745d382d5d4aee0a00f
Mattias Olsson <mattiaso@crepido.com>
2019-11-18 10:18:23 +0100

    Added some basic stuff to TW

---
+++ Library/Helpers/IThreadWatcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Helpers
+{
+    public interface IThreadWatcher
+    {
+        /// <summary>
+        /// Is this thread running for the cacheName?
+        /// </summary>
+        /// <param name="cacheName"></param>
+        /// <returns></returns>
+        bool IsRunning(string cacheName); 
+    }
+}
---
+++ Library/Helpers/ThreadWatcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Helpers
+{
+    // This is the event subscriber. 
+
+    public class ThreadWatcher : IThreadWatcher
+    {
+        private Dictionary<string, bool> Threads = new Dictionary<string, bool>();
+
+        public bool IsRunning(string cacheName)
+        {
+            // Check the dictionary to see if this thread is running. 
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
